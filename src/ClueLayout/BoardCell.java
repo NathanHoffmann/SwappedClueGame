9c7629d08a77e1e8fc9c559937d701b2fb9f8ff7
Jordan <jpontill@mines.edu>
2015-02-17 19:13:47 -0700

    Initial commit

---
+++ src/ClueLayout/BoardCell.java
@@ -0,0 +1,59 @@
+package ClueLayout;
+
+public class BoardCell {
+	private int row;
+	private int column;
+	public BoardCell(int column, int row) {
+		super();
+		this.column = column;
+		this.row = row;
+	}
+
+	public int getRow() {
+		return row;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + column;
+		result = prime * result + row;
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BoardCell other = (BoardCell) obj;
+		if (column != other.column)
+			return false;
+		if (row != other.row)
+			return false;
+		return true;
+	}
+	public void setRow(int row) {
+		this.row = row;
+	}
+	public int getColumn() {
+		return column;
+	}
+	public void setColumn(int column) {
+		this.column = column;
+	}
+
+	@Override
+	public String toString() {
+		return "BoardCell [row=" + row + ", column=" + column + "]";
+	}
+
+	
+	
+
+	
+	
+
+}

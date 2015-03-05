fe41b5c317355d3d5da6d92b904292cdc926b9f4
unknown <faruna@CT156-34.adit.mines.edu>
2015-03-04 18:38:39 -0700

    implemented loadCardTests

--- src/clueGame/GameSetUpTest.java
+++ src/clueGame/GameSetUpTest.java
@@ -20,27 +20,49 @@ import com.sun.prism.paint.Color;
 
 public class GameSetUpTest {
 
+	ClueGame game;
 	@Before
 	public void setUp() throws Exception {
+	    game=new ClueGame();
+		game.loadConfigFiles();
 	}
 
-	@Test
-	public void loadPeopletest() throws FileNotFoundException , BadConfigFormatException {
-		ClueGame game=new ClueGame();
-		game.loadConfigFiles();
 	
-	}
 	@Test
 	public void loadCardtest() throws FileNotFoundException , BadConfigFormatException {
-		ClueGame game=new ClueGame();
-		game.loadConfigFiles();
-		Assert.assertEquals(21, game.getCards().size());
+		//test if The deck contains the correct total number of cards
+		//random number for the cards size
+		Assert.assertEquals(23, game.getCards().size());
+		
+		//test if The deck contains the correct number of cards of each type
+		int weapon=0;
+		int person=0;
+		int rooms=0;
 		for (int i=0; i < game.getCards().size();i++){
 			if(game.getCards().get(i).getType()==cardType.WEAPON){
-				
+				weapon++;
+			}
+			else if(game.getCards().get(i).getType()==cardType.PERSON){
+				person++;
+			}
+			if(game.getCards().get(i).getType()==cardType.ROOM){
+				rooms++;
 			}
 		}
-		
+		Assert.assertEquals(8, weapon);
+		Assert.assertEquals(6, person);
+		Assert.assertEquals(9, rooms);
+		// select one room, one weapon, and one person, and ensure the deck contains each of those
+		Card card=new Card();
+		card.setName("Miss Scarlet");
+		card.setType(cardType.PERSON);
+		Assert.assertTrue(game.getCards().contains(card));
+		card.setName("TNT");
+		card.setType(cardType.WEAPON);
+		Assert.assertTrue(game.getCards().contains(card));
+		card.setName("Library");
+		card.setType(cardType.ROOM);
+		Assert.assertTrue(game.getCards().contains(card));
 		
 	}
 

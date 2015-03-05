e5d673a036c5d12f172dac0e955f1c7c71df4930
unknown <faruna@CT156-34.adit.mines.edu>
2015-03-04 17:38:38 -0700

    created GameSetUpTest Class

---
+++ src/clueGame/GameSetUpTest.java
@@ -0,0 +1,19 @@
+package clueGame;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GameSetUpTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
